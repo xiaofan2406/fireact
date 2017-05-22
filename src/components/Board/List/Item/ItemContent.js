90d21fab25d9a463165ea8cec63915ce91eeb796
Xiaofan Wu <xiaofan2406@gmail.com>
2017-05-22 22:32:10 +1000

    item name

--- src/components/Board/List/Item/ItemContent.js
+++ src/components/Board/List/Item/ItemContent.js
@@ -3,23 +3,34 @@ import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import withCss from 'react-jss';
 import { compose } from 'utils';
+import classnames from 'classnames';
 
 const css = {
-  name: {
+  container: {
     display: 'inline-block',
     padding: ' 0px 8px',
     lineHeight: 1,
     cursor: 'default',
     userSelect: 'none',
     flex: 1
+  },
+  name: {
+    color: 'black',
+    '&.placeholder': {
+      color: 'grey'
+    }
   }
 };
 
 function ItemContent({ classes, item, onContentClick }) {
   console.log('render item content');
+  const nameClasses = classnames({
+    [classes.name]: true,
+    placeholder: !item.name
+  });
   return (
-    <span className={classes.name} onClick={onContentClick}>
-      {item.name}
+    <span className={classes.container} onClick={onContentClick}>
+      <span className={nameClasses}>{item.name || 'New To-Do'}</span>
     </span>
   );
 }
