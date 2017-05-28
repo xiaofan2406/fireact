57ec6c203f183585b1979cf960f0df9ee9f1e30f
Xiaofan Wu <xiaofan2406@gmail.com>
2017-05-28 15:32:46 +1000

    greensock for the win!

--- src/components/Board/List/Item/ItemDisplay.js
+++ src/components/Board/List/Item/ItemDisplay.js
@@ -2,9 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import withCss from 'react-jss';
-import classnames from 'classnames';
-import { compose } from 'utils';
 import { colors, theme, spacing, variables } from 'styles';
+import { TweenLite } from 'gsap';
 
 import ItemCheckbox from './ItemCheckbox';
 import ItemName from './ItemName';
@@ -15,6 +14,7 @@ const css = {
   ItemDisplay: {
     display: 'flex',
     flexDirection: 'column',
+    backgroundColor: 'transparent',
     '&.isEditing': {
       backgroundColor: colors.white,
       boxShadow: theme.boxShadow,
@@ -36,36 +36,71 @@ const css = {
   bottomRow: {}
 };
 
-function ItemDisplay({ classes, item, getContainer }) {
-  console.log('render ItemDisplay');
-  const classNames = classnames({
-    [classes.ItemDisplay]: true,
-    isEditing: item.isEditing
-  });
-  return (
-    <div className={classNames}>
-      <div className={classes.topRow}>
-        <ItemCheckbox item={item} />
-        {item.isEditing
-          ? <ItemName item={item} getContainer={getContainer} />
-          : <ItemMeta item={item} />}
+@withCss(css)
+@observer
+class ItemDisplay extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    item: PropTypes.object.isRequired,
+    getContainer: PropTypes.func.isRequired
+  };
 
+  componentDidMount() {
+    const { item } = this.props;
+    if (item.isEditing && this.container) {
+      TweenLite.fromTo(
+        this.container,
+        0.2,
+        { css: { backgroundColor: colors.white } },
+        { className: '+=isEditing', lazy: true }
+      );
+    }
+  }
+
+  componentDidUpdate() {
+    const { item } = this.props;
+    if (item.isEditing && this.container) {
+      TweenLite.fromTo(
+        this.container,
+        0.2,
+        { css: { backgroundColor: colors.white } },
+        { className: '+=isEditing', lazy: true }
+      );
+    } else {
+      TweenLite.fromTo(
+        this.container,
+        0.2,
+        { css: { backgroundColor: 'transparent' } },
+        { className: '-=isEditing', lazy: true }
+      );
+    }
+  }
+
+  containerRef = ref => {
+    this.container = ref;
+  };
+
+  render() {
+    const { classes, item, getContainer } = this.props;
+    console.log('render ItemDisplay');
+
+    return (
+      <div className={classes.ItemDisplay} ref={this.containerRef}>
+        <div className={classes.topRow}>
+          <ItemCheckbox item={item} />
+          {item.isEditing
+            ? <ItemName item={item} getContainer={getContainer} />
+            : <ItemMeta item={item} />}
+
+        </div>
+        <div className={classes.bottomRow}>
+          {item.isEditing
+            ? <ItemNotes item={item} getContainer={getContainer} />
+            : null}
+        </div>
       </div>
-      <div className={classes.bottomRow}>
-        {item.isEditing
-          ? <ItemNotes item={item} getContainer={getContainer} />
-          : null}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-ItemDisplay.propTypes = {
-  classes: PropTypes.object.isRequired,
-  item: PropTypes.object.isRequired,
-  getContainer: PropTypes.func.isRequired
-};
-
-const enhance = compose(withCss(css), observer);
-
-export default enhance(ItemDisplay);
+export default ItemDisplay;
